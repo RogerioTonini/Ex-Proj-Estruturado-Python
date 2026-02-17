57ad01419fddfcac4d2080685f44378a14f1d343
Rogerio <rogerio.tonini@gmail.com>
2026-02-17 17:01:57 -0300

    style: Correções feitas com base na biblio flake8

--- app/pipeline/extract.py
+++ app/pipeline/extract.py
@@ -1,5 +1,5 @@
 import glob  # biblioteca para encontrar arquivos e diretórios usando padrões
-import os  # biblioteca para manipulação de arquivos e diretórios
+import os    # biblioteca para manipulação de arquivos e diretórios
 from typing import List
 
 import pandas as pd
@@ -8,27 +8,29 @@ import pandas as pd
 def extract_from_excel(path: str) -> List[pd.DataFrame]:
     """
     Função para ler arquivos de uma pasta e retornar uma Lista de DataFrames
-
-    args: input_path: str - caminho da pasta onde estão os arquivos a serem lidos
-
+    args: input_path: str - caminho pasta onde estão os arquivos a serem lidos
     return: Lista de DataFrames contendo os dados dos arquivos lidos
     """
+    # Encontrar todos os arquivos Excel na pasta especificada
     all_files = glob.glob(
         os.path.join(path, '*.xlsx')
-    )       # Encontrar todos os arquivos Excel na pasta especificada
+    )
+    # Lista para armazenar os DataFrames
     data_frame_list = (
         []
-    )                                      # Lista para armazenar os DataFrames
+    )
 
+    # Ler cada arquivo Excel e adicionar o DataFrame à lista
     for file in all_files:
         data_frame_list.append(
             pd.read_excel(file)
-        )                                  # Ler cada arquivo Excel e adicionar o DataFrame à lista
+        )
 
+    # Lançar um erro se nenhum arquivo for encontrado
     if not data_frame_list:
         raise ValueError(
             'Nenhum arquivo Excel encontrado na pasta especificada.'
-        )  # Lançar um erro se nenhum arquivo for encontrado
+        )
 
     return data_frame_list
 
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -5,15 +5,15 @@ from app.pipeline.transform import contact_data_frames
 df_1 = pd.DataFrame({"col_1": [1, 2], "col_2": [3, 4]})
 df_2 = pd.DataFrame({"col_1": [5, 6], "col_2": [7, 8]})
 
+
 def test_contact_data_frames():
     """
     Use o arrange, act e assert para testar a função contact_data_frames
     """
-    #arrange
+    # arrange
     data_frame_list = [df_1, df_2]
-    
+
     # act
     df = contact_data_frames(data_frame_list)
 
     assert df.shape == (4, 2)
-    
